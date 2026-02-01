f9fb7a3085c519f5c8b5999ae10f312b7d8d5dea
Mauro Sarti <mauro.sarti.9292@gmail.com>
2026-02-01 13:56:24 -0300

    Initial version configs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,6 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -6,7 +8,19 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
+
+    // Ktor
+    alias(libs.plugins.kotlin.serialization)
+
+    // Room
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+
+    // Unit testing
+    alias(libs.plugins.mockative)
+
+    // ktlint
+    alias(libs.plugins.ktlint)
 }
 
 kotlin {
@@ -15,23 +29,30 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
-        iosArm64(),
-        iosSimulatorArm64()
+        iosArm64(), // Dispositivo real
+        iosSimulatorArm64() // Simulador en Mac Apple Silicon
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
         }
     }
-    
+
     jvm()
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
+
+            // DI
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+
+            // Ktor and Coil
+            implementation(libs.ktor.client.okhttp)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -40,27 +61,92 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // General
+            implementation(libs.kotlinx.serialization.json)
+
+            // ViewModel
+            implementation(libs.lifecycle.viewmodel)
+            implementation(libs.kotlinx.coroutines.core)
+
+            // Navigation
+            implementation(libs.navigation.compose)
+
+            // DI
+            api(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+
+            // Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor)
+
+            // Ktor
+            implementation(libs.bundles.ktor)
+
+            // Room
+            implementation(libs.room.runtime)
+            implementation(libs.sqlite.bundled)
+
+            // Compottie (Lottie KMP lib) --> https://github.com/alexzhirkevich/compottie/tree/standalone-main
+            implementation(libs.compottie)
         }
         commonTest.dependencies {
+            // Unit testing
             implementation(libs.kotlin.test)
+            implementation(kotlin("test-annotations-common"))
+            implementation(libs.assertk)
+            implementation(libs.mockative)
+            implementation(libs.turbine)
+
+            // UI testing
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+        }
+        nativeMain.dependencies {
+            // Ktor and Coil
+            implementation(libs.ktor.client.darwin)
         }
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.kotlinx.coroutines.swing)
+
+            // Ktor and Coil
+            implementation(libs.ktor.client.okhttp)
+        }
+        // Room-2
+        commonMain {
+            kotlin.srcDir("build/generated/ksp/metadata")
+        }
+    }
+    // Unit testing
+    mockative {
+        sourceSets {
+            commonMain {
+            }
         }
     }
 }
 
 android {
     namespace = "com.cemede.cemede"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk =
+        libs.versions.android.compileSdk
+            .get()
+            .toInt()
 
     defaultConfig {
         applicationId = "com.cemede.cemede"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk =
+            libs.versions.android.minSdk
+                .get()
+                .toInt()
+        targetSdk =
+            libs.versions.android.targetSdk
+                .get()
+                .toInt()
         versionCode = 1
         versionName = "1.0"
     }
@@ -81,7 +167,16 @@ android {
 }
 
 dependencies {
-    debugImplementation(libs.compose.uiTooling)
+    debugImplementation(compose.uiTooling)
+
+    // Room
+    ksp(libs.room.compiler)
+    add("kspCommonMainMetadata", libs.room.compiler) // Esto genera el código compartido
+//    add("kspAndroid", libs.room.compiler)
+//    add("kspIosSimulatorArm64", libs.room.compiler)
+//    add("kspIosX64", libs.room.compiler)
+//    add("kspIosArm64", libs.room.compiler)
+//    add("kspDesktop", libs.room.compiler)
 }
 
 compose.desktop {
@@ -94,4 +189,29 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
+}
+
+// Room
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+ktlint {
+    version.set("1.5.0") // ktlint (Pinterest) core version
+    android.set(true) // Android-specific rules
+    outputToConsole.set(true) // Shows errors in console
+    ignoreFailures.set(false) // The build fails if issues are found
+
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+    }
+
+    filter {
+        exclude("**/build/**") // Exclude build folder
+    }
+
+    // To execute ktlint you can run:
+    // ./gradlew ktlintCheck
+    // ./gradlew ktlintFormat
 }
