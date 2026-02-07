f669b08134cb2f5739aaec11b75313befc138d1b
Mauro Sarti <mauro.sarti.9292@gmail.com>
2026-02-07 17:47:32 -0300

    UrlProvider added

---
+++ composeApp/src/commonMain/kotlin/com/cemede/cemede/data/UrlProvider.kt
@@ -0,0 +1,19 @@
+package com.cemede.cemede.data
+
+object UrlProvider {
+    val professorUrls =
+        mapOf(
+            "Macarena" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1173195328&single=true&output=csv",
+            "Tomas" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=437089319&single=true&output=csv",
+            "Manuel" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1256799923&single=true&output=csv",
+            "Santiago" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=863130999&single=true&output=csv",
+            "Melany" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=2116023249&single=true&output=csv",
+            "Gonzalo" to
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1161548379&single=true&output=csv",
+        )
+}
--- composeApp/src/commonMain/kotlin/com/cemede/cemede/data/repository/ProfessorRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/cemede/cemede/data/repository/ProfessorRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.cemede.cemede.data.repository
 
+import com.cemede.cemede.data.UrlProvider
 import com.cemede.cemede.data.data_base.model.ProfessorEntity
 import com.cemede.cemede.data.mapper.CsvParser
 import com.cemede.cemede.domain.data_base.CemedeDataBase
@@ -18,26 +19,10 @@ class ProfessorRepositoryImpl(
     override fun getProfessorDetail(id: Int): Flow<Professor> = cemedeDataBase.getProfessorDetail(id)
 
     override suspend fun syncProfessors(): CoroutineResult<Unit> {
-        val professorUrls =
-            mapOf(
-                "Macarena" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1173195328&single=true&output=csv",
-                "Tomas" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=437089319&single=true&output=csv",
-                "Manuel" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1256799923&single=true&output=csv",
-                "Santiago" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=863130999&single=true&output=csv",
-                "Melany" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=2116023249&single=true&output=csv",
-                "Gonzalo" to
-                    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMB5vup1yN-1eTHa0XtxGDVQKJT6aNsJi3zHf1dWj2qvGc1ybP0X-Igk-jzFBYFSMCesr1sd2Q3bxn/pub?gid=1161548379&single=true&output=csv",
-            )
-
         val professors = mutableListOf<ProfessorEntity>()
         val allStudents = mutableListOf<com.cemede.cemede.data.data_base.model.StudentEntity>()
 
-        for ((name, url) in professorUrls) {
+        for ((name, url) in UrlProvider.professorUrls) {
             when (val result = csvDataSource.getProfessorData(url)) {
                 is CoroutineResult.Success -> {
                     val professorEntity = ProfessorEntity(name = name)
