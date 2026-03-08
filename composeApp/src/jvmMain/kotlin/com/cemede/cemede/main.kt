76ed62582b499baa6a17728997a0d963f3ad74fb
Mauro Sarti <mauro.sarti.9292@gmail.com>
2026-03-08 19:59:57 -0300

    All platforms working

--- composeApp/src/jvmMain/kotlin/com/cemede/cemede/main.kt
+++ composeApp/src/jvmMain/kotlin/com/cemede/cemede/main.kt
@@ -2,12 +2,16 @@ package com.cemede.cemede
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.cemede.cemede.di.initKoin
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "CeMeDe",
-    ) {
-        App()
+fun main() {
+    initKoin()
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "CeMeDe",
+        ) {
+            App()
+        }
     }
 }
