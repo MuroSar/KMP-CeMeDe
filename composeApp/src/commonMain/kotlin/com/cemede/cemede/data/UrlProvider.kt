fd50800aed152e7abf562c6d2aae2d6343fab6aa
Mauro Sarti <mauro.sarti.9292@gmail.com>
2026-04-02 14:10:37 -0300

    CEMEDE URLs

--- composeApp/src/commonMain/kotlin/com/cemede/cemede/data/UrlProvider.kt
+++ composeApp/src/commonMain/kotlin/com/cemede/cemede/data/UrlProvider.kt
@@ -2,43 +2,43 @@ package com.cemede.cemede.data
 
 object UrlProvider {
     val partnersTab =
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1094196425&single=true&output=csv"
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1094196425&single=true&output=csv"
 
     val protocolsTab =
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=336008188&single=true&output=csv"
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=336008188&single=true&output=csv"
 
     val staffMembersWorkingScheduleTab =
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=2069095849&single=true&output=csv"
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=2069095849&single=true&output=csv"
 
     val staffMemberTabs =
         mapOf(
             "Tomas" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=437089319&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=437089319&single=true&output=csv",
             "Macarena" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1173195328&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1173195328&single=true&output=csv",
             "Manuel" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1256799923&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1256799923&single=true&output=csv",
             "Santiago" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=863130999&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=863130999&single=true&output=csv",
             "Melany" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=2116023249&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=2116023249&single=true&output=csv",
             "Gonzalo" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1161548379&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1161548379&single=true&output=csv",
         )
 
     val staffMemberScheduleTabs =
         mapOf(
             "Tomas" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=244000281&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1483813507&single=true&output=csv",
             "Macarena" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1916387988&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1971999308&single=true&output=csv",
             "Manuel" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1162687633&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=1880670073&single=true&output=csv",
             "Santiago" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=1786982631&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=863037847&single=true&output=csv",
             "Melany" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=67060029&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=2065773473&single=true&output=csv",
             "Gonzalo" to
-                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3hvq3Qa5mXc3EusVsf7W4hol8N-73EEi5Obdo6PiqHo-YivlJByelUNfE6YvzWRIaAsNWUzU4X7R_/pub?gid=327501789&single=true&output=csv",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRPiFDnNLVzEVuyxx5D701nooUKEg_qy-4Y0wktedjD8r0ozd4fZhhsbYXx7MYiizQ1eOOPw600YTJ/pub?gid=899930537&single=true&output=csv",
         )
 }
