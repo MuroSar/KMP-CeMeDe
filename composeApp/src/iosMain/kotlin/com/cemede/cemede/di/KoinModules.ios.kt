8a0910fb8560b8d14eb5d90eb5a59d4dff45b5b1
Mauro Sarti <mauro.sarti.9292@gmail.com>
2026-05-02 10:37:14 -0300

    iOS devices run fix

--- composeApp/src/iosMain/kotlin/com/cemede/cemede/di/KoinModules.ios.kt
+++ composeApp/src/iosMain/kotlin/com/cemede/cemede/di/KoinModules.ios.kt
@@ -5,25 +5,41 @@ import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.cemede.cemede.data.data_base.CemedeDB
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import org.koin.dsl.module
-import platform.Foundation.NSHomeDirectory
+// Importaciones necesarias para las rutas de iOS
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
-actual val platformModule =
-    module {
-        // HttpClientEngine
-        single<HttpClientEngine> {
-            Darwin.create()
-        }
-        // Room database, CemedeDatabase
-        single<CemedeDB> {
-            Room
-                .databaseBuilder<CemedeDB>(
-                    name = NSHomeDirectory() + "/${CemedeDB.DATABASE_NAME}",
-                ).setDriver(BundledSQLiteDriver())
-                .setQueryCoroutineContext(Dispatchers.IO)
-//            .fallbackToDestructiveMigration(true)
-                .build()
-        }
+@OptIn(ExperimentalForeignApi::class)
+actual val platformModule = module {
+    // HttpClientEngine
+    single<HttpClientEngine> {
+        Darwin.create()
     }
+
+    // Room database
+    single<CemedeDB> {
+        // 1. Buscamos la ruta legal para escribir archivos (Documents)
+        val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null
+        )
+
+        // 2. Armamos el path completo: Documents/Cemede.db
+        val dbFilePath = documentDirectory?.path + "/" + CemedeDB.DATABASE_NAME
+
+        Room.databaseBuilder<CemedeDB>(
+            name = dbFilePath, // <--- CAMBIAMOS NSHomeDirectory por dbFilePath
+        )
+            .setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
+            .build()
+    }
+}
